43990a79e4b326fafa50ef9f27c7a97dfd8d349d
diego <3dvolt.info@gmail.com>
2020-05-21 12:31:00 +0200

    add chat message anche channel

---
+++ server.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
+
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers
+} = require('./utils/users');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Run when client connects
+io.on('connection', socket => {
+  socket.on('joinRoom', ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+    socket.join(user.room);
+    // Send users and room info
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getRoomUsers(user.room)
+    });
+  });
+
+  //send audio message
+  socket.on("audioMessage", function(msg) {
+    const user = getCurrentUser(socket.id);
+    //socket.broadcast.emit("audioMessage", msg);
+    socket.broadcast.to(user.room).emit("audioMessage", msg);
+  });
+
+
+  // Listen for chatMessage
+  socket.on('chatMessage', msg => {
+    const user = getCurrentUser(socket.id);
+    io.to(user.room).emit('message', formatMessage(user.username, msg));
+  });
+
+  // Runs when client disconnects
+  socket.on('disconnect', () => {
+    const user = userLeave(socket.id);
+      // Send users and room info
+      io.to(user.room).emit('roomUsers', {
+        room: user.room,
+        users: getRoomUsers(user.room)
+      })
+    });
+});
+
+const PORT = process.env.PORT || 80;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
